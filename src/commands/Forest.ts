64e9471203fedd664d4c4d2a1da377bede3c2c7f
ssMMiles <github@milesmoonlove.com>
2022-05-09 01:23:10 +0000

    feat: add pagination for leaderboards

--- src/commands/Forest.ts
+++ src/commands/Forest.ts
@@ -6,13 +6,23 @@ import {
   EmbedBuilder,
   ISlashCommand,
   MessageBuilder,
+  SimpleError,
   SlashCommandBuilder,
-  SlashCommandContext
+  SlashCommandContext,
+  SlashCommandIntegerOption
 } from "interactions.ts";
 import { Guild } from "../models/Guild";
 
+type LeaderboardButtonState = {
+  page: number;
+};
+
+const MEDAL_EMOJIS = ["🥇", "🥈", "🥉"];
+
 export class Forest implements ISlashCommand {
-  public builder = new SlashCommandBuilder("forest", "See the tallest trees in the whole forest.");
+  public builder = new SlashCommandBuilder("forest", "See the tallest trees in the whole forest.").addIntegerOption(
+    new SlashCommandIntegerOption("page", "Leaderboard page to display.").setMinValue(1).setMaxValue(10)
+  );
 
   public handler = async (ctx: SlashCommandContext): Promise<void> => {
     return ctx.reply(await buildLeaderboardMessage(ctx));
@@ -25,24 +35,70 @@ export class Forest implements ISlashCommand {
       async (ctx: ButtonContext): Promise<void> => {
         return ctx.reply(await buildLeaderboardMessage(ctx));
       }
+    ),
+    new Button(
+      "forest.back",
+      new ButtonBuilder().setEmoji({ name: "◀️" }).setStyle(2),
+      async (ctx: ButtonContext<LeaderboardButtonState>): Promise<void> => {
+        if (!ctx.state) return;
+
+        ctx.state.page--;
+        return ctx.reply(await buildLeaderboardMessage(ctx));
+      }
+    ),
+    new Button(
+      "forest.next",
+      new ButtonBuilder().setEmoji({ name: "▶️" }).setStyle(2),
+      async (ctx: ButtonContext<LeaderboardButtonState>): Promise<void> => {
+        if (!ctx.state) return;
+
+        ctx.state.page++;
+        return ctx.reply(await buildLeaderboardMessage(ctx));
+      }
     )
   ];
 }
 
-async function buildLeaderboardMessage(ctx: SlashCommandContext | ButtonContext): Promise<MessageBuilder> {
-  let description = "";
+async function buildLeaderboardMessage(
+  ctx: SlashCommandContext | ButtonContext<LeaderboardButtonState>
+): Promise<MessageBuilder> {
+  const state: LeaderboardButtonState =
+    ctx instanceof SlashCommandContext
+      ? { page: ctx.options.has("page") ? Number(ctx.options.get("page")?.value) : 1 }
+      : (ctx.state as LeaderboardButtonState);
 
-  const trees = await Guild.find().sort({ size: -1 }).limit(10);
+  let description = `*The tallest trees of all.*\n\n`;
+
+  const start = (state.page - 1) * 10;
+
+  const trees = await Guild.find().sort({ size: -1 }).skip(start).limit(11);
+
+  if (trees.length === 0) return SimpleError("This page is empty.");
 
   for (let i = 0; i < 10; i++) {
     if (i === trees.length) break;
+    const pos = i + start;
 
     const tree = trees[i];
 
-    description += `${i < 3 ? `**${i + 1}**` : `${i + 1}`}. \`\`${tree.name}\`\` - ${tree.size}ft\n`;
+    description += `${pos < 3 ? `${MEDAL_EMOJIS[i]}` : `\`\`${pos + 1}${pos < 9 ? " " : ""}\`\``} - \`\`${
+      tree.name
+    }\`\` - ${tree.size}ft\n`;
+  }
+
+  const actionRow = new ActionRowBuilder().addComponents(
+    await ctx.manager.components.createInstance("forest.refresh", state)
+  );
+
+  if (state.page > 1) {
+    actionRow.addComponents(await ctx.manager.components.createInstance("forest.back", state));
+  }
+
+  if (trees.length > 10) {
+    actionRow.addComponents(await ctx.manager.components.createInstance("forest.next", state));
   }
 
   return new MessageBuilder()
     .addEmbed(new EmbedBuilder().setTitle("Forest").setDescription(description))
-    .addComponents(new ActionRowBuilder().addComponents(await ctx.manager.components.createInstance("forest.refresh")));
+    .addComponents(actionRow);
 }
--- src/commands/Leaderboard.ts
+++ src/commands/Leaderboard.ts
@@ -6,15 +6,31 @@ import {
   EmbedBuilder,
   ISlashCommand,
   MessageBuilder,
+  SimpleError,
   SlashCommandBuilder,
-  SlashCommandContext
+  SlashCommandContext,
+  SlashCommandIntegerOption
 } from "interactions.ts";
 
+const builder = new SlashCommandBuilder(
+  "leaderboard",
+  "See a leaderboard of contributors to this server's tree."
+).addIntegerOption(
+  new SlashCommandIntegerOption("page", "Leaderboard page to display.").setMinValue(1).setMaxValue(10)
+);
+builder.setDMEnabled(false);
+
+type LeaderboardButtonState = {
+  page: number;
+};
+
+const MEDAL_EMOJIS = ["🥇", "🥈", "🥉"];
+
 export class Leaderboard implements ISlashCommand {
-  public builder = new SlashCommandBuilder("leaderboard", "See a leaderboard of contributors to this server's tree.");
+  public builder = builder;
 
   public handler = async (ctx: SlashCommandContext): Promise<void> => {
-    if (!ctx.game) return ctx.reply("Use /plant to plant a tree for your server first.");
+    if (ctx.game === null) return ctx.reply("Use /plant to plant a tree for your server first.");
 
     return ctx.reply(await buildLeaderboardMessage(ctx));
   };
@@ -23,31 +39,81 @@ export class Leaderboard implements ISlashCommand {
     new Button(
       "leaderboard.refresh",
       new ButtonBuilder().setEmoji({ name: "🔄" }).setStyle(2),
-      async (ctx: ButtonContext): Promise<void> => {
+      async (ctx: ButtonContext<LeaderboardButtonState>): Promise<void> => {
+        if (!ctx.state?.page) {
+          ctx.state = { page: 1 };
+        }
+
+        return ctx.reply(await buildLeaderboardMessage(ctx));
+      }
+    ),
+    new Button(
+      "leaderboard.back",
+      new ButtonBuilder().setEmoji({ name: "◀️" }).setStyle(2),
+      async (ctx: ButtonContext<LeaderboardButtonState>): Promise<void> => {
+        if (!ctx.state) return;
+
+        ctx.state.page--;
+        return ctx.reply(await buildLeaderboardMessage(ctx));
+      }
+    ),
+    new Button(
+      "leaderboard.next",
+      new ButtonBuilder().setEmoji({ name: "▶️" }).setStyle(2),
+      async (ctx: ButtonContext<LeaderboardButtonState>): Promise<void> => {
+        if (!ctx.state) return;
+
+        ctx.state.page++;
         return ctx.reply(await buildLeaderboardMessage(ctx));
       }
     )
   ];
 }
 
-async function buildLeaderboardMessage(ctx: SlashCommandContext | ButtonContext): Promise<MessageBuilder> {
-  if (!ctx.game) throw new Error("Game data missing.");
+async function buildLeaderboardMessage(
+  ctx: SlashCommandContext | ButtonContext<LeaderboardButtonState>
+): Promise<MessageBuilder> {
+  if (ctx.game === null) throw new Error("Game data missing.");
 
-  let description = "";
+  const state: LeaderboardButtonState =
+    ctx instanceof SlashCommandContext
+      ? { page: ctx.options.has("page") ? Number(ctx.options.get("page")?.value) : 1 }
+      : (ctx.state as LeaderboardButtonState);
+
+  let description = `*These users have contributed the most towards watering \`\`${ctx.game.name}\`\`.*\n\n`;
 
   const contributors = ctx.game.contributors.sort((a, b) => b.count - a.count);
 
-  for (let i = 0; i < 10; i++) {
-    if (i === contributors.length) break;
+  if (contributors.length === 0) return SimpleError("This page is empty.");
+
+  const start = (state.page - 1) * 10;
+
+  for (let i = start; i < start + 10; i++) {
+    if (i === contributors.length) {
+      if (i === start) description = `This page is empty.`;
+      break;
+    }
 
     const contributor = contributors[i];
 
-    description += `${i < 3 ? `**${i + 1}**` : `${i + 1}`}. 💧${contributor.count} <@${contributor.userId}>\n`;
+    description += `${i < 3 ? `${MEDAL_EMOJIS[i]}` : `\`\`${i + 1}${i < 9 ? " " : ""}\`\``} - 💧${
+      contributor.count
+    } <@${contributor.userId}>\n`;
+  }
+
+  const actionRow = new ActionRowBuilder().addComponents(
+    await ctx.manager.components.createInstance("leaderboard.refresh", state)
+  );
+
+  if (state.page > 1) {
+    actionRow.addComponents(await ctx.manager.components.createInstance("leaderboard.back", state));
+  }
+
+  if (state.page < Math.ceil(contributors.length / 10)) {
+    actionRow.addComponents(await ctx.manager.components.createInstance("leaderboard.next", state));
   }
 
   return new MessageBuilder()
     .addEmbed(new EmbedBuilder().setTitle("Leaderboard").setDescription(description))
-    .addComponents(
-      new ActionRowBuilder().addComponents(await ctx.manager.components.createInstance("leaderboard.refresh"))
-    );
+    .addComponents(actionRow);
 }
